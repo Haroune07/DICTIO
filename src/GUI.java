7b1d0821ffcc1ccc7f3181d34ca937f9e5276f31
Haroune Belhachani <harounebelh45@gmail.com>
2024-09-25 18:37:00 -0400

    DICTIO version terminal

---
+++ src/GUI.java
@@ -0,0 +1,20 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GUI extends JFrame {
+
+    public GUI(){
+        super("DICTIO");
+
+        setSize(500,500);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+    }
+
+}
